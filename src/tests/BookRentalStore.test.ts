d080a8dc589c1ed9c4fe52cf7108e22a7635160d
Sarvesh Srivastava <hercules51@Sarveshs-MacBook-Air.local>
2023-05-31 23:42:45 +0530

    Second Story Design Complete

--- src/tests/BookRentalStore.test.ts
+++ src/tests/BookRentalStore.test.ts
@@ -3,7 +3,7 @@ import { Book, BookType } from "../model/book";
 import { Customer } from "../model/customer";
 
 describe('BookRentalStore', () => {
-  it('should calculate charges correctly for Story 1', () => {
+  it('should calculate charges correctly for Story 2', () => {
     // Create a customer with rented books
     const customer = new Customer('1', 'Sarvesh Sri', [
       new Book('1', 'Book 1', 3, BookType.Regular),
